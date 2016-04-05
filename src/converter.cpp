adefc86428746215c35ef7c7fde291e43599b483
Daniel Mercier <mercierdaniel91@gmail.com>
2016-04-05 23:02:29 +0200

    Corrected an error with mass matrix computation

--- src/converter.cpp
+++ src/converter.cpp
@@ -13,20 +13,25 @@ arma::mat Converter::get_invmass(const Object::State &o1, const Object::State &o
   glm::mat3 I1 = o1.inverseInertiaTensor;
   glm::mat3 I2 = o2.inverseInertiaTensor;
   arma::mat invmass =
-    { { o1.inverseMass, 0             , 0             , 0       , 0       , 0       , 0       , 0       , 0       , 0             , 0             , 0          },
-      { 0             , o1.inverseMass, 0             , 0       , 0       , 0       , 0       , 0       , 0       , 0             , 0             , 0          },
-      { 0             , 0             , o1.inverseMass, 0       , 0       , 0       , 0       , 0       , 0       , 0             , 0             , 0          },
-      { 0             , 0             , 0             , I1[0][0], I1[0][1], I1[0][2], 0       , 0       , 0       , 0             , 0             , 0          },
-      { 0             , 0             , 0             , I1[1][0], I1[1][1], I1[1][2], 0       , 0       , 0       , 0             , 0             , 0          },
-      { 0             , 0             , 0             , I1[2][0], I1[2][1], I1[2][2], 0       , 0       , 0       , 0             , 0             , 0          },
-      { 0             , 0             , 0             , 0       , 0       , 0       , I2[0][0], I2[0][1], I2[0][2], 0             , 0             , 0          },
-      { 0             , 0             , 0             , 0       , 0       , 0       , I2[1][0], I2[1][1], I2[1][2], 0             , 0             , 0          },
-      { 0             , 0             , 0             , 0       , 0       , 0       , I2[2][0], I2[2][1], I2[2][2], 0             , 0             , 0          },
-      { 0             , 0             , 0             , 0       , 0       , 0       , 0       , 0       , 0       , o2.inverseMass, 0             , 0          },
-      { 0             , 0             , 0             , 0       , 0       , 0       , 0       , 0       , 0       , 0             , o2.inverseMass, 0          },
-      { 0             , 0             , 0             , 0       , 0       , 0       , 0       , 0       , 0       , 0             , 0             , o2.inverseMass } };
+    { { o1.inverseMass, 0             , 0             , 0       , 0       , 0       , 0             , 0                 , 0             , 0      , 0       , 0        },
+      { 0             , o1.inverseMass, 0             , 0       , 0       , 0       , 0             , 0                 , 0             , 0      , 0       , 0        },
+      { 0             , 0             , o1.inverseMass, 0       , 0       , 0       , 0             , 0                 , 0             , 0      , 0       , 0        },
+      { 0             , 0             , 0             , I1[0][0], I1[0][1], I1[0][2], 0             , 0                 , 0             , 0      , 0       , 0        },
+      { 0             , 0             , 0             , I1[1][0], I1[1][1], I1[1][2], 0             , 0                 , 0             , 0      , 0       , 0        },
+      { 0             , 0             , 0             , I1[2][0], I1[2][1], I1[2][2], 0             , 0                 , 0             , 0      , 0       , 0        },
+      { 0             , 0             , 0             , 0       , 0       , 0       , o2.inverseMass, 0                 , 0             , 0      , 0       , 0        },
+      { 0             , 0             , 0             , 0       , 0       , 0       , 0             , o2.inverseMass    , 0             , 0      , 0       , 0        },
+      { 0             , 0             , 0             , 0       , 0       , 0       , 0             , 0                 , o2.inverseMass, 0      , 0       , 0        },
+      { 0             , 0             , 0             , 0       , 0       , 0       , 0             , 0                 , 0             ,I2[0][0], I2[0][1], I2[0][2] },
+      { 0             , 0             , 0             , 0       , 0       , 0       , 0             , 0                 , 0             ,I2[1][0], I2[1][1], I2[1][2] },
+      { 0             , 0             , 0             , 0       , 0       , 0       , 0             , 0                 , 0             ,I2[2][0], I2[2][1], I2[2][2] }
+    };
 
   return invmass;
+}
+
+static arma::mat get_invmass_only(const Object::State &o1, const Object::State &o2){
+
 }
 
 void Converter::get_velocity(arma::vec velocity, glm::dvec3 &v1, glm::dvec3 &a1, glm::dvec3 &v2, glm::dvec3 &a2){
